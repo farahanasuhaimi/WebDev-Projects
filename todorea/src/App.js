b49960bd8e955413b545714d11b747116ef4b858
farahanasuhaimi <farahanams@gmail.com>
2021-03-26 10:43:18 +0800

    update app and form

--- todorea/src/App.js
+++ todorea/src/App.js
@@ -11,7 +11,13 @@ function App() {
       <header>
         <h1>Farahana's Todo List</h1>
       </header>
-      <Form setInputText={setInputText} />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        set
+        setInputText={setInputText}
+      />
       <TodoList />
     </div>
   );
